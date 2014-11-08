66dcd16cc45b8a9083b3c00bc29dceed1918828a
Kunal Sharma <ksharma8@terpmail.umd.edu>
2014-11-08 14:54:38 -0500

    recleaned data

--- datasets/extraction_cleaning_scripts/large_local/to_localities.py
+++ datasets/extraction_cleaning_scripts/large_local/to_localities.py
@@ -2,7 +2,7 @@
 
 dept_data = {}
 
-for year in range(1985, 2013):
+for year in range(1984, 2013):
 	curr_file = open(str(year)+".csv", 'r')
 	for line in curr_file:
 		line_arr = line.split(",")
@@ -17,10 +17,10 @@ for year in range(1985, 2013):
 
 for key in dept_data:
 	curr_output = open("output/" + key.replace(' ', '_') +".csv", 'w')
-	curr_output.write("Year,State,Months,Population,Violent crime total,Murder and nonnegligent Manslaughter,Forcible rape,Robbery,Aggravated assault,Property crime total,Burglary,Larceny-theft,Motor vehicle theft,Violent Crime rate,Murder and nonnegligent manslaughter rate,Forcible rape rate,Robbery rate,Aggravated assault rate,Property crime rate,Burglary rate,Larceny-theft rate,Motor vehicle theft rate,")
+	curr_output.write("Year,State,Months,Population,Violent crime total,Murder and nonnegligent Manslaughter,Forcible rape,Robbery,Aggravated assault,Property crime total,Burglary,Larceny-theft,Motor vehicle theft,Violent Crime rate,Murder and nonnegligent manslaughter rate,Forcible rape rate,Robbery rate,Aggravated assault rate,Property crime rate,Burglary rate,Larceny-theft rate,Motor vehicle theft rate,\n")
 
 	for line in dept_data[key]:
-		curr_output.write(line)
+		curr_output.write(line[:-3].replace("'", '').replace('\n','') + '\n')
 
 	curr_output.close()
 
